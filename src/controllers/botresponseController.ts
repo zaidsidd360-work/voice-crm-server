457f0c499a2e9909444a061e496c9b4194490b18
zaidsidd360 <zaidsidd360@gmail.com>
2025-01-20 18:23:13 +0530

    changes to bot-response

--- src/controllers/botresponseController.ts
+++ src/controllers/botresponseController.ts
@@ -2,31 +2,38 @@ import { Request, Response } from "express";
 import Airtable from "airtable";
 
 const base = new Airtable({
-	apiKey: "patu3bTCvjKcj1dAH.232bd2feb82fe4be336fd4c10ccc7cc9a14d93485b799a61320f01f9ba1a33e2",
+	apiKey: "patu3bTCvjKcj1dAH.14bdd086d57abbd25e028e8225211f1aca60f9ce95e0ce0526a47d19a4522379",
 }).base("appFBqZs4uvwCGzkm");
 
 export const sendDataToAirtable = async (req: Request, res: Response) => {
-	const { name, email } = req.body.args;
-
-	// console.log(req.body);
-	console.log(name, email);
-
-	if (!name || !email) {
-		return res.status(400).json({ error: "Name and email are required" });
+	const { message } = req.body;
+	if (message.type === "tool-calls") {
+		const vapiFn = message.toolCalls[0].function;
+		try {
+			const record = await base("Lead").create(vapiFn.arguments);
+			console.log(record);
+			return res.status(200).json({
+				success: true,
+				result: "Your data has been saved successfully.",
+			});
+		} catch (error) {
+			return res
+				.status(500)
+				.json({ result: "Failed to save your data." });
+		}
 	}
+	console.log(message.toolCalls[0].function);
+	// console.log(name, email);
+
+	// if (!name || !email) {
+	// 	return res.status(400).json({ error: "Name and email are required" });
+	// }
 
 	try {
-		const record = await base("Lead").create({
-			Name: name,
-			Email: email,
-			Phone: "(987) 563-9459",
-			"Business Name": "New Corp",
-		});
-
-		res.status(200).json({
-			message: "Data sent to Airtable successfully",
-			record,
-		});
+		// res.status(200).json({
+		// 	message: "Data sent to Airtable successfully",
+		// 	record,
+		// });
 	} catch (error) {
 		console.error("Error sending data to Airtable:", error);
 		res.status(500).json({ error: "Failed to send data to Airtable" });
